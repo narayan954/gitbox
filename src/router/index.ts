02ded651f1112226677f2d5fe8feb55c6bc49074
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-07-21 23:19:09 +0530

    Initialize followers

---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/repositories',
+    name: 'Repository',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Repositories.vue')
+  },
+  {
+    path: '/users',
+    name: 'Users',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Users.vue')
+  },
+  {
+    path: '/activities',
+    name: 'Activities',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Activities.vue')
+  }
+]
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+export default router
---
+++ src/stores/user.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+import { reactive, ref } from 'vue'
+
+export const userStore = defineStore('user', () => {
+  const userName = ref('')
+  const contributions = ref(2129)
+
+  const isLoading = ref(false)
+
+  const userData = reactive<any>([])
+  const orgsData = reactive<any>([])
+  const starredData = reactive<any>([])
+  const reposData = reactive<any>([])
+  const followersData = reactive<any>([])
+
+  return{
+    userName, contributions, isLoading, userData, orgsData, starredData, reposData, followersData
+  }
+})
