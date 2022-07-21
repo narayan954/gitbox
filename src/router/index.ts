901ba4b86d3e12096580df631cb20709753732a6
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-07-21 23:19:09 +0530

    Initialize activities-data

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/repositories',
+    name: 'Repository',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Repositories.vue')
+  },
+  {
+    path: '/activities',
+    name: 'Activities',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Activities.vue')
+  }
+]
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+export default router
