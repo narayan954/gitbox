0e2b9a513983361a2ab747dfb43cdcf96b7e0f45
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-07-21 23:19:07 +0530

    Initialize parent-main

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Main />
+    <Accessibilities @childSendName="parentGetName($event)" />
+    <Repositories />
+    <SideBar />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Main from "./components/Main.vue";
+import Accessibilities from "./components/Accessibilities.vue";
+import Repositories from "./components/Repositories.vue";
+import SideBar from "./components/SideBar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Main,
+    Accessibilities,
+    Repositories,
+    SideBar,
+    Footer,
+  },
+};
+</script>
